9f8435625dc672ca5a5d93b35a28db61920d9236
krishna mandal <krishnamandal30870@gmail.com>
2024-11-28 23:05:06 +0530

    first

---
+++ src/components/Banner.jsx
@@ -0,0 +1,20 @@
+
+
+function Banner(){
+
+    return(
+        <>
+       <div className="max-w-screen-2x1 container mx-auto md:px-20 px-4 flex">
+          
+       <div className=" w-full md:w-1/2">Left</div>
+
+
+
+           <div className=" w-full md:w-1/2">Right</div>
+
+
+       </div>
+        </>
+    )
+}
+export default Banner
